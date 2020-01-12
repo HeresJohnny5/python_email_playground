4870a287a5122d6e67b0e45159e967a99e4f68b1
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-01-12 10:13:04 -0500

    Import Template and Path libraries. Add index.html file to dynamically customize email content.

--- email_sender.py
+++ email_sender.py
@@ -1,11 +1,14 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'John Doe'
 email['to'] = 'recipient_email_address'
 email['subject'] = 'Email Subject Line'
-email.set_content('email content')
+email.set_content(html.substitute(name='Nacho'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
